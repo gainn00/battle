16863af2d11411dcb8c9e7a45afd707cf6b140d8
TH-0123 <darkmiline123@gmail.com>
2026-05-22 16:26:56 +0900

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+import Wizard.*;
+import Spell.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Harry h = new Harry("해리",100,90);
+		Hermione m = new Hermione("헤르미온느",90,120);
+		Ron r = new Ron("론",110,80);
+		Draco d = new Draco("말포이",95,100);
+		
+		
+		
+		
+
+	}
+
+}
